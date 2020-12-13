9bf367d955bacf6c9dc64a81354f76b44c741426
bvidalneofox <v_andres01@hotmail.com>
2020-12-13 18:15:08 -0300

    primer commir

---
+++ src/components/Formulario.js
@@ -0,0 +1,73 @@
+import { useState, Fragment } from "react";
+import { calcularTotal } from "../helpers";
+
+const Formulario = (props) => {
+    // Destructuracion
+    const { cantidad, guardarCantidad, plazo, guardarPlazo, guardarTotal } = props;
+
+    // Definir State
+    const [error, guardarError] = useState(false);
+
+    // Cuando el usuario hace submit
+    const calcularPrestamo = e => {
+        e.preventDefault();
+
+        // Validar
+        if (cantidad === 0 || plazo === '') {
+            guardarError(true);
+            return;
+        }
+
+        // Eliminar error previo
+        guardarError(false);
+
+        // Realizar Cotizacion
+        const total = calcularTotal(cantidad, plazo);
+        
+        // Una vez calculado guardar todal
+        guardarTotal(total);
+    }
+
+    return (
+        <Fragment>
+            <form onSubmit={calcularPrestamo}>
+                <div className="row">
+                    <div>
+                        <label>Cantidad Prestamo</label>
+                        <input
+                            className="u-full-width"
+                            type="number"
+                            placeholder="Ejemplo: 3000"
+                            onChange={e => guardarCantidad(parseInt(e.target.value))}
+                        />
+                    </div>
+                    <div>
+                        <label>Plazo para Pagar</label>
+                        <select
+                            className="u-full-width"
+                            onChange={e => guardarPlazo(parseInt(e.target.value))}
+                        >
+                            <option value="">Seleccionar</option>
+                            <option value="3">3 meses</option>
+                            <option value="6">6 meses</option>
+                            <option value="12">12 meses</option>
+                            <option value="24">24 meses</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="Calcular"
+                            className="button-primary u-full-width"
+                        />
+                    </div>
+                </div>
+            </form>
+
+            { (error) ? <p  className="error">Todos los campos son obligatorios</p> : null }
+            
+        </Fragment>
+    );
+}
+
+export default Formulario;
